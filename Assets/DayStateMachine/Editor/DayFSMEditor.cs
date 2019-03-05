82733917fef0eecb81f2a7af20bf7b0f2f87cb81
Daydreel <daydreel@gmail.com>
2019-03-05 13:15:55 +0100

    Imported Tools: NodeBaseEditor, Singleton, Timer

---
+++ Assets/DayStateMachine/Editor/DayFSMEditor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class DayFSMEditor : NodeBasedEditor
+{
+    List<FiniteStateMachine> FSMs;
+
+    [MenuItem("Tool/ Day FSM Editor")]
+    private static void OpenWindow()
+    {
+        DayFSMEditor window = GetWindow<DayFSMEditor>();
+        window.titleContent = new GUIContent("Day FSM Editor");
+    }
+}
